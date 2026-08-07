4e1f43591e73e3b84ee6849c97d7081dace96828
Lucas Ramos <ramoslucasivan@gmail.com>
2026-08-07 20:02:10 -0300

    feat: extraer auth card reutilizable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import RegisterForm from "./components/auth/RegisterForm";
+import AuthCard from "./components/auth/AuthCard";
+import "./App.css";
 
 function App() {
-  return <RegisterForm />;
+  const handleLogin = () => {
+    console.log("Click en Ingresar");
+  };
+
+  const handleRegister = () => {
+    console.log("Click en Registrar");
+  };
+
+  return (
+    <main className="app-page">
+      <AuthCard onLogin={handleLogin} onRegister={handleRegister} />
+    </main>
+  );
 }
 
 export default App;
