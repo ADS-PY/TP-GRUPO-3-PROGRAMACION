dadbc9eea9c9bded70d116bfe97b27bbbe9ac933
Lucas Ramos <ramoslucasivan@gmail.com>
2026-08-07 22:08:42 -0300

    feat: conectar boton registrar con registro

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../components/auth/RegisterForm";
+import "./RegisterPage.css";
+
+export default function RegisterPage() {
+  return (
+    <main className="register-page-view">
+      <RegisterForm />
+    </main>
+  );
+}
