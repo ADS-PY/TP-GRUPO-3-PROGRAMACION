6e13dc476baf295ea6aedb4d40b68017b81d7a9d
Marcelo Peralta <aulas.virtuales.marnel@gmail.com>
2026-08-22 21:17:14 -0300

    Vaciar rama main

--- src/hooks/index.js
+++
@@ -1,1 +0,0 @@
-export { useStats } from './useStats';
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/Home.jsx
-import { LandingPage } from '../components';
-
-function Home() {
-  return <LandingPage />;
-}
-
-export default Home;
